28fa6eecc51136b8e714c8f88781650d6e4d8203
LyricLy <hanson.twentytwo@gmail.com>
2017-05-26 10:15:10 +1200

    Fix crashing if save.json doesn't exist or is empty

--- IdlePY.py
+++ IdlePY.py
@@ -162,20 +162,24 @@ def next_command():
             print("game shell home")
     else:
         print("Invalid command.")
+#load game save
+try:
+    with open('save.json', 'r+') as f:
+        data = json.load(f)
+    #if the old save doesn't have variables from a newer version, the newer variables won't be deleted
+    for variable in data:
+        user[variable] = data[variable]
+    #load unlocked commands into command list for help command
+    if "update" in user["commands"]:
+        command_list["update"] = "Updates the system, if there is a new update available."
+    if "buy" in user["commands"]:
+        command_list["buy"] = "Allows you to buy software to help you get more points. Subcommands: view"
+    if "ls" in user["commands"]:
+        command_list["ls"] = "Discover what files are on your system so that you can poke around with other software."    
+except (FileNotFoundError, json.decoder.JSONDecodeError):
+    #if save.json doesn't exist or is empty, skip loading instead of crashing
+    pass
 
-with open('save.json', 'r+') as f:
-    data = json.load(f)
-#if the old save doesn't have variables from a newer version, the newer variables won't be deleted
-for variable in data:
-    user[variable] = data[variable]
-#load unlocked commands into command list for help command
-if "update" in user["commands"]:
-    command_list["update"] = "Updates the system, if there is a new update available."
-if "buy" in user["commands"]:
-    command_list["buy"] = "Allows you to buy software to help you get more points. Subcommands: view"
-if "ls" in user["commands"]:
-    command_list["ls"] = "Discover what files are on your system so that you can poke around with other software."
-    
 print("IdlePY shell version a" + str(user["version"]))
 print("------------------------")
 time.sleep(1)
