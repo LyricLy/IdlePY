d1721064c601244b74b9c887ca1fbad283924424
LyricLy <hanson.twentytwo@gmail.com>
2017-06-06 10:57:57 +1200

    small fixes

--- main.py
+++ main.py
@@ -7,10 +7,15 @@ try:
     with open('save.json', 'r+') as f:
         data = json.load(f)
     #if the old save doesn't have variables from a newer version, the newer variables won't be deleted
-    for variable in data["user"]:
-        user[variable] = data["user"][variable]
-    for variable in data["software_prices"]:
-        software_prices[variable] = data["software_prices"][variable]
+    try:
+        for variable in data["user"]:
+            user[variable] = data["user"][variable]
+        for variable in data["software_prices"]:
+            software_prices[variable] = data["software_prices"][variable]
+    except KeyError:
+        #old save
+        for variable in data:
+            user[variable] = data[variable]
     #load unlocked commands into command list for help command
     if "update" in user["commands"]:
         command_list["update"] = "Updates the system, if there is a new update available."
@@ -18,8 +23,8 @@ try:
         command_list["buy"] = "Allows you to buy software to help you get more points. Subcommands: view"
     if "ls" in user["commands"]:
         command_list["ls"] = "Discover what files are on your system so that you can poke around with other software."    
-except Exception:
-    #if save.json doesn't exist or is empty, skip loading instead of crashing
+except (FileNotFoundError, json.JSONDecodeError):
+    #if save.json doesn't exist or is empty
     pass
 
 print("IdlePY shell version a" + str(user["version"]))
